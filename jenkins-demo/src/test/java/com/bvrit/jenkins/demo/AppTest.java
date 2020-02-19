0d74e56df40c54aeb1828f885d8ebd9b68efdaf7
ramanujdas <ramanujds9@gmail.com>
2020-02-19 22:47:31 +0530

    commit-2 added test cases

--- jenkins-demo/src/test/java/com/bvrit/jenkins/demo/AppTest.java
+++ jenkins-demo/src/test/java/com/bvrit/jenkins/demo/AppTest.java
@@ -1,38 +1,33 @@
 package com.bvrit.jenkins.demo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+    
 {
+	@Test
+	public void test1() {
+		assertEquals(10, 10);
+	}
+	
+	@Test
+	public void test2() {
+		assertThrows(ArithmeticException.class, ()->{int x=5/0;});
+	}
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
+   
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
